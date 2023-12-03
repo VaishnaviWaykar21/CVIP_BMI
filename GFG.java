e8766370043823cfc66c75fef81181e6d37056d6
Vaishnavi Waykar <119470671+VaishnaviWaykar21@users.noreply.github.com>
2023-12-03 21:53:57 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,37 @@
+import java.io.*; 
+import java.util.*;
+class GFG 
+{ 
+    public static double BMI(double height, double weight) 
+    { 
+        double bmi = weight / Math.pow(height, 2); 
+        return bmi; 
+    } 
+    
+    public static void main(String[] args) 
+    { 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input weight in kilogram: ");
+        double weight = sc.nextDouble();
+        System.out.print("Input height in meters: ");
+        double height = sc.nextDouble();
+        
+        
+        
+        double bmi = BMI(height, weight); 
+        System.out.print("The BMI is " + bmi + " so "); 
+        
+        
+        if (bmi < 18.5) 
+            System.out.print("underweight"); 
+  
+        else if (bmi >= 18.5 && bmi < 24.9) 
+            System.out.print("Healthy"); 
+  
+        else if (bmi >= 24.9 && bmi < 30) 
+            System.out.print("overweight"); 
+  
+        else if(bmi >= 30) 
+            System.out.print("Suffering from Obesity"); 
+    } 
+}
